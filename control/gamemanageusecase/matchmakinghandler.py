e4bceb6df3de6f7fb96b5f935caca6fdcde30799
Angelo <angelo.damiani.ch@gmail.com>
2017-07-23 22:41:18 +0200

    MatchMakingHandler decorator delete

--- control/gamemanageusecase/matchmakinghandler.py
+++ control/gamemanageusecase/matchmakinghandler.py
@@ -2,8 +2,6 @@ import Pyro4
 
 from foundations.oophelpers.singleton import SingletonMetaclass
 
-@Pyro4.behavior(instance_mode="single")
-@Pyro4.expose
 class MatchMakingHandler(metaclass=SingletonMetaclass):
     def makeNewGame(self, clientid: str, modeid: str, isranked: bool):
         pass
