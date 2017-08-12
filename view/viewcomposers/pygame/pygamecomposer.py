1d65fcb3d15e3f413eac8d0b86beb76f46c254db
castogio <gioacchino.castorio@icloud.com>
2017-08-12 21:41:33 +0200

    configurazione mediante file delle impostazioni del viewcomposer

--- view/viewcomposers/pygame/pygamecomposer.py
+++ view/viewcomposers/pygame/pygamecomposer.py
@@ -23,31 +23,19 @@ class PyGameComposer(IViewComposer):
         # inizializzazione di pygame e dello schermo
         pygame.init()
 
-        # dati di screen
-        # TODO prendere dipensioni da file di configurazione
-        width = 1280
-        height = 720
-        screenobject: object = pygame.display.set_mode((width, height))
-        windowname = "BYOB - Bring Your Own Bomb!"
-        pygame.display.set_caption(windowname)
-
+        # settings di finestra
+        self._windowname: str = None
+        self._windowwidth: int = 0
+        self._windowheight: int = 0
+        self._framerate: int = 0
         self._frameratemanager: Clock = pygame.time.Clock()
-        self._framerate: int = 60  # TODO prendere framerate da file di configurazione
-
-        # TODO prendere dipensioni da file di configurazione
-        self._screen: Screen = Screen(screenobject, width, height, self._framerate)
-
-        # path base per i media
-        self._basemediapath: str = "foundations/media/"  # TODO prendere da file di configurazione
+        self._screen: Screen = None
 
         # inizializzazione joypad
         self._initJoyPad()
 
-        # semaforo di mutua esclusione
-        self._semaphore: Lock = Lock()
-
-        # template mostrato a schermo
-        self._currenttemplate: ITemplate = None
+        # path dei media content
+        self._basemediapath: str = None
 
         # template disponibili
         self._templates: Dict[Templates, ITemplate] = {
@@ -57,8 +45,21 @@ class PyGameComposer(IViewComposer):
             Templates.CHOOSEBOB: PyGameBobSelectionTemplate()
         }
 
+        # template mostrato a schermo correntemente
+        self._currenttemplate: ITemplate = None
+
+        # semaforo di mutua esclusione (gestione multithreading)
+        self._semaphore: Lock = Lock()
+
     def init(self, eventhandlercallback: Callable[[object, GameMessages, Dict[str, any]], None]):
         self._observercallback = eventhandlercallback
+
+        # bundle dello screen
+        screenobject: object = pygame.display.set_mode((self._windowwidth, self._windowheight))
+        self._screen = Screen(screenobject, self._windowwidth, self._windowheight, self._framerate)
+
+        # mostra titolo finestra
+        pygame.display.set_caption(self._windowname)
 
     def show(self, chosenview: Templates):
         # retrieve e inizializzazione del template richiesto
@@ -97,3 +98,43 @@ class PyGameComposer(IViewComposer):
         joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
         if joysticks:
             joysticks[0].init()
+
+    @property
+    def windowcaption(self) -> str:
+        return self._windowname
+
+    @windowcaption.setter
+    def windowcaption(self, cap: str):
+        self._windowname = cap
+
+    @property
+    def windowwidth(self) -> int:
+        return self._windowwidth
+
+    @windowwidth.setter
+    def windowwidth(self, width: int):
+        self._windowwidth = width
+
+    @property
+    def windowheight(self) -> int:
+        return self._windowheight
+
+    @windowheight.setter
+    def windowheight(self, height: int):
+        self._windowheight = height
+
+    @property
+    def framerate(self) -> int:
+        return self._windowheight
+
+    @framerate.setter
+    def framerate(self, fr: int):
+        self._framerate = fr
+
+    @property
+    def mediapath(self) -> str:
+        return self._basemediapath
+
+    @mediapath.setter
+    def mediapath(self, path: str):
+        self._basemediapath = path
